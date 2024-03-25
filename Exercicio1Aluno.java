9c02c96d5b4c13faab41625659892351846dd7e7
Eudes Pereira <eudes.pereira22@gmail.com>
2024-03-25 19:40:10 -0300

    Exercicio1t

---
+++ Exercicio1Aluno.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+
+public class Aluno {
+    private String nome;
+    private double nota1, nota2, nota3;
+
+    public Aluno(String nome, double nota1, double nota2, double nota3) {
+        this.nome = nome;
+        this.nota1 = nota1;
+        this.nota2 = nota2;
+        this.nota3 = nota3;
+    }
+
+    public double calcularMedia() {
+        return (nota1 + nota2 + nota3) / 3;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o nome do aluno:");
+        String nome = scanner.nextLine();
+
+        System.out.println("Digite a nota da primeira prova:");
+        double nota1 = scanner.nextDouble();
+
+        System.out.println("Digite a nota da segunda prova:");
+        double nota2 = scanner.nextDouble();
+
+        System.out.println("Digite a nota da terceira prova:");
+        double nota3 = scanner.nextDouble();
+
+        Aluno aluno = new Aluno(nome, nota1, nota2, nota3);
+        double media = aluno.calcularMedia();
+
+        System.out.println("Nome do aluno: " + nome);
+        System.out.println("Média do aluno: " + media);
+        
+        scanner.close();
+    }
+}
